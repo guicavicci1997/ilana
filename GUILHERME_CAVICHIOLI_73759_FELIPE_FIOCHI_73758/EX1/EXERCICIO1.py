850956279078332482878cc5d00125b04ddd0771
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-27 22:12:26 -0300

    ajustes

--- GUILHERME_CAVICHIOLI_73759_FELIPE_FIOCHI_73758/EX1/EXERCICIO1.py
+++ GUILHERME_CAVICHIOLI_73759_FELIPE_FIOCHI_73758/EX1/EXERCICIO1.py
@@ -2,6 +2,18 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread("lena.jpg", 0)
+#Lendo a imagem
+img = cv2.imread("lena.jpg")
 
-cv2.imshow("lena", img)
+#Aplicando filtro blur
+blur = cv2.GaussianBlur(img, (15,15),0)
+
+
+#sobelX = cv2.Sobel(img, cv2.CV_64F,1,0,ksize=5)
+#sobelY = cv2.Sobel(img, cv2.CV_64F, 0,1, ksize=5)
+#sobel = cv2.addWeighted(sobelX, 1, sobelY, 1 ,0)
+
+result = cv2.addWeighted(img, 1, blur, 1, 0)
+cv2.subtract (img, img2, dst, mask, dtype)
+
+cv2.imshow("lena", result)
--- GUILHERME_CAVICHIOLI_73759_FELIPE_FIOCHI_73758/EX2/EXERCICIO2.py
+++ GUILHERME_CAVICHIOLI_73759_FELIPE_FIOCHI_73758/EX2/EXERCICIO2.py
@@ -4,4 +4,19 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread("coins.png", 0)
 
-cv2.imshow("coins", img)
+#img2 = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+
+title = 'binaria'
+
+images = [img, thresh1]
+plt.subplot(),plt.imshow(thresh1,'gray')
+plt.title(title)
+
+plt.show()
+
+
+##################
+
+
